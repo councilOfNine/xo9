a9e9a11bcddaf774a7da25433ddf7dd0caabd18a
Mike Torres <6453torres@gmail.com>
2024-03-28 20:57:21 -0500

    update testing deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -16,7 +17,15 @@ const config = {
 		// instead of creating a single one for the entire app.
 		// if `edge` is true, this option cannot be used
 		split: false,
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
